8bc262eebb1a27d2293c7321d659e4a30f96eea2
NANDHINIDIGAJARLA <153707996+nandy2004@users.noreply.github.com>
2025-08-12 10:01:10 -0500

    Add files via upload

---
+++ DatatypeBool.py
@@ -0,0 +1,12 @@
+print("Data Type-Boolean")
+Nandu="Hello I'm Nandhini"
+Saagu="Hello I'm Sagarika"
+a=True
+b=False
+print(len(Nandu))
+print(len(Saagu))
+if(len(Nandu)==len(Saagu)):
+    print(a)
+else:
+    print(b)
+print(type(a))
