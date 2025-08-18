6840781cfb80d64bb9fe2dc43162fd41e403de9a
K. Baby Pravallika <kpravallikababy@gmail.com>
2025-08-18 18:37:31 +0530

    Add files via upload

---
+++ Logical.py
@@ -0,0 +1,22 @@
+#logical operators
+a="python is one of the  most popular programming languages"
+b=" The code to understand how python "
+print(len(a))
+print(len(b))
+#logical AND
+if ((a==a))and((b==b)):
+ print("true")
+else:
+ print("flase")
+ #logical OR
+ if((a==16))or((b==79)):
+  print("true")
+ else:
+  print("flase")
+  #logical NOT
+if(not(a==9))>((b==47)):
+  print(" its is not satified")
+else:
+  print("its satified")
+
+
