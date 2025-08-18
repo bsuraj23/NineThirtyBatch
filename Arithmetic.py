e45389c87d3204f5fddfa8bf77934910b17e8503
K. Baby Pravallika <kpravallikababy@gmail.com>
2025-08-18 18:22:53 +0530

    Add files via upload

---
+++ Arithmetic.py
@@ -0,0 +1,35 @@
+#add
+a=40
+b=90
+c=a+b
+print("addition value",c)
+#sub
+a=50
+b=70
+c=a-b
+print("sub value",c)
+#mul
+a=70
+b=20
+c=a*b
+print("mul value",c)
+# module divi
+a=60
+b=50
+c=a%b
+print("modulus value",c)
+#divi
+a=30
+b=50
+c=a/b
+print("divi value",c)
+#flour divi
+a=28
+b=49
+c=a//b
+print("flour divi",c)
+#power
+a=70
+b=48
+c=a**b
+print("power value",c)
