7936704dbdf9964192bd02547296cfc50e35e38e
K. Baby Pravallika <kpravallikababy@gmail.com>
2025-08-18 18:32:50 +0530

    Add files via upload

---
+++ Relational.py
@@ -0,0 +1,19 @@
+#Relational 
+print("realtion operatoes")
+a="baby"
+b="ammu"
+#equal to
+if(a==b):
+   print("true")
+else:
+   print("flase")
+#not equal to
+if(a!=b):
+   print("true")
+else:
+   print("flase") 
+#greater than
+if((a>b)):
+   print("true") 
+else:
+   print("flase") 
