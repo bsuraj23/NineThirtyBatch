59de3da3aaf43eeee7eb6191ef2322d5c3617ded
Adithya kumar <adithyakonjerla@gmail.com>
2025-08-18 21:48:08 +0530

    Add files via upload

---
+++ arithmeticoperator.py
@@ -0,0 +1,24 @@
+print("Arithmetic Operators")
+#Arithmetic operator +
+adhi=9
+hemu=4
+add=adhi+hemu
+print("The sum is",add)
+#Arithmetic operators -
+sub=adhi-hemu
+print("The Subtraction is",sub)
+#Arithmetic operator *
+into=adhi*hemu
+print("The Multiplication is",into)
+#Arithmetic operator /
+divide=adhi/hemu
+print("The division is",divide)
+#Arithmetic operator %
+modulas=adhi%hemu
+print("The Modulas division is",modulas)
+#Arithmetic operator //
+floor=adhi//hemu
+print("The floor division is",floor)
+#Arithmetic operator **
+power=adhi**hemu
+print("The Power value is",power)
