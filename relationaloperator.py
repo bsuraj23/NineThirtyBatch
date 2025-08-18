738fec0ab73259db7f0084829b33368f2b5e0930
Adithya kumar <adithyakonjerla@gmail.com>
2025-08-18 22:15:45 +0530

    Add files via upload

---
+++ relationaloperator.py
@@ -0,0 +1,18 @@
+anu =22
+adhi =21
+if anu > adhi:
+    print("anu is greater")
+    print("anu is",anu,"adhi is",adhi)
+
+
+#if anu < adhi
+
+anu = 22
+adhi =22 
+if anu > adhi:
+    print("anu is greater or equal")
+    print("anu is",anu,"adhi is",adhi)
+
+if anu < adhi:
+    print("anu is greater or equal")
+    print("anu is",anu,"adhi is",adhi)
