725ebce547e472c4158ebff68649fad12dfcca12
Keerthananethikoppula <153708650+Keerthananethikoppula@users.noreply.github.com>
2025-08-27 19:06:02 +0530

    Add files via upload

---
+++ logical operators.py
@@ -0,0 +1,10 @@
+# Logical OPertors are AND OR NOT
+a= True
+b=False
+c= a and b 
+if a and b:
+    print("it is logical AND")
+if a or b:
+    print("it is logical OR")
+if 1==0:
+    print("it is true")               
