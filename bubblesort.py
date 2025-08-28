94a03a438f21be03746b1adfa783672d0fa3b5cd
Abhishek4128 <abhishekmandal0204@gmail.com>
2025-08-28 12:10:55 +0530

    i am adding a bubblesort code

--- bubblesort.py
+++ bubblesort.py
@@ -1,7 +1,8 @@
-temp=[22,7,4,17,19,89,9,11,16,55]
+temp=[2,4,74,44,999,82,99,56]
 n=len(temp)-1
 for i in range(n):
-  for j in range(0,n-i):
+    for j in range(0,n-i):
         if temp[j]>temp[j+1]:
             temp[j],temp[j+1]=temp[j+1],temp[j]
-print("SORTED LIST IS:",temp)
+            i=i+1
+print(temp)
