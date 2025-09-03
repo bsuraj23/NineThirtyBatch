237773ae68d65c94b91c6b76071ad6a58c8ef25a
Keerthananethikoppula <153708650+Keerthananethikoppula@users.noreply.github.com>
2025-09-03 23:33:25 +0530

    Add files via upload
    operations on list

---
+++ list1.py
@@ -0,0 +1,14 @@
+
+list= ["1", "2", 23, 45.45454]
+print(list)
+print(list[0])
+print(list[-2])
+list[1] ="A"
+list[2] = "kee"
+print(list)
+list.append("teju")
+print(list)
+list = list 
+list.append("srilekha")
+list[3] = "suri"
+print("After changing ",list)
