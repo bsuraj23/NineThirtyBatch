0776208a407d62f21a936565730d0c6d924e5cdc
Abhishek4128 <abhishekmandal0204@gmail.com>
2025-09-05 11:19:15 +0530

    i am adding a code of arthimetic operators

---
+++ arthimeticoperators.py
@@ -0,0 +1,28 @@
+print("THIS ARE THE ARTHIMETIC OPERATORS")
+a=26
+b=13
+"""we are performing addition operation"""
+sum=a+b
+print(sum)
+"""we are perfoming subtraction operation"""
+sub=a-b
+print(sub)
+"""we are perfoming multiply operation"""
+mul=a*b
+print(mul)
+"""we are perfoming division operation"""
+div=a/b
+print(div)
+"""we are perfoming modulus operation"""
+mod=a%b
+print(mod)
+"""we are perfoming floor division operation"""
+c=26
+d=9
+floor=c//d
+print(floor)
+"""we are perfoming power operation"""
+val=2
+val2=3
+power=2**3
+print(power)
