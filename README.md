466139817defb0ee8f63643019487b25eec073e8
Farhan <mohdfarhanuddin002@gmail.com>
2025-09-09 01:06:04 +0530

    Initial commit with my python files

---
+++ README.md
@@ -0,0 +1,1 @@
+my theory answers for today's questions
---
+++ datatypesmath.py
@@ -0,0 +1,9 @@
+a=10
+b=3
+print("sum:",a+b)
+print("difference:",a-b)
+print("product:",a*b)
+print("division:",a/b)
+print("floor division:",a//b)
+print("modulus:",a%b)
+print("exponent:",a**b)
