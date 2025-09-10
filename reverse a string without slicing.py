675b16aa204ac8b4c4e4e8ef4bcef97d781a68c7
anumolusreya3 <anumolusreya3@gmail.com>
2025-09-10 10:45:50 +0530

    Add files via upload

---
+++ reverse a string without slicing.py
@@ -0,0 +1,18 @@
+# s = "sreya"
+# #i = len(s)-1
+# rev = ""
+# # while i>=0:
+# #     rev = rev + s[i]
+# #     i-=1
+# # print(rev)
+# for c in s:
+#     rev = c+rev
+# print(rev)    
+# numbers = [12, 45, 67, 23, 89, 34]
+
+# largest = numbers[0]   # assume first element is largest
+# for num in numbers:
+#     if num > largest:
+#         largest = num
+
+# print("Largest number:", largest)
