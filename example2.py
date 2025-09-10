ac91179c85644ca3371a8a657d72dd2edf41abd3
NandhiniDigajarla <nandhinidigajarla@gmail.com>
2025-09-10 05:48:09 -0500

    Added the program to find secondlargest number in the given list

---
+++ example2.py
@@ -0,0 +1,56 @@
+'''#!/usr/bin/python
+ # Measure some strings:
+names  = ['mohan', 'sunil', 'uday']
+for temp in names:
+     print(temp)
+
+#cat 3
+#window 6
+#defenestrate 12
+
+#range([start], stop[, step])
+
+for i in range(1, 100,4):
+    print(i)'''
+
+''''for n in range(2, 10):
+    for x in range(2, n):
+         if n % x == 0:
+             print(n, 'equals', x, '*', n//x)
+             break
+         else:
+         # loop fell through without finding a factor
+            print(n,'is a prime number')'''
+
+'''a=90
+b=8
+if(a==90 and b==81):
+    print("a is 90 and b is 8")
+
+elif (a==90 and b==71):
+    print("a= 90 and b=7")
+
+else:
+    print("this is just else")
+
+if(a==901 or b==81):
+    print("a is 90 and b is 8")
+
+elif (a==901 or b==7):
+    print("a= 90 and b=7")
+
+else:
+    print("this is just else")'''
+
+
+'''d = {'coords': (10, 20)}
+d2 = {'rgb': (255, 255, 0)}
+d3 = {'version': (3, 8)}
+d4 = {'date': (2025, 7, 10)}
+d5 = {'grades': ('A', 'B', 'C')}
+print(d)
+print(d2)
+print(d3)
+print(d4)
+print(d5)'''
+
