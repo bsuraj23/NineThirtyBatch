89ad9f7d6086d3507ceef63e97d09cfa83d875f5
unknown <bsuraj23@gmail.com>
2025-09-11 09:53:01 +0530

    adding PPt topics

--- class1/datatypes.py
+++ class1/datatypes.py
@@ -14,3 +14,12 @@ b = 454545454
 c = 34.56656565
 
 print("value of b is " + str(b))
+
+#code for adding two numbers 
+a = input("Enter first number: ")
+b = input("Enter second number: ")
+print("The sum of two numbers is " + str(int(a) + int(b)))
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(a))
