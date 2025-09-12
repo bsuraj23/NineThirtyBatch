4e5db5fc5dd170569c98fec5505c8a6212c28739
Farhan <mohdfarhanuddin002@gmail.com>
2025-09-12 22:24:28 +0530

    deleted 16 outdated python files to clean up Farhan_branch

--- slicingskipvalue.py
+++
@@ -1,5 +0,0 @@
-word = "amazing"
-
-print(word[::2])   # 'aazn' - every second character (step=2)
-print(word[1::2])  # 'mzig' - every second character starting from index 1
-print(word[::-2])  # 'gima' - reversed string, every second character
--- tuplemethods.py
+++
@@ -1,6 +0,0 @@
-# Tuple Methods in Python
-
-my_tuple = (10, 20, 30, 20, 40, 20)
-
-print(my_tuple.count(20))   # Counts how many times 20 appears (Output: 3)
-print(my_tuple.index(30))   # Finds the index of first occurrence of 30 (Output: 2)
