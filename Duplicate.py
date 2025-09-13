eb15ff7e3d21e86010c0add3d2a447ae0357d9e3
K. Baby Pravallika <kpravallikababy@gmail.com>
2025-09-13 12:40:48 +0530

    Add files via upload

---
+++ Duplicate.py
@@ -0,0 +1,6 @@
+Baby ={1,2,5,6,2,1,5,6.6,9}
+prav =[]
+for i in Baby:
+    if i not in prav:
+         prav.append (i)
+         print("list",prav)
---
+++ dictionaries.py
@@ -0,0 +1,4 @@
+dict1={"name":'ammu', "id":547,"emial id":"kpravallikababy@gmail.com"}
+dict2={ "flowers":" sun flowers","color":"yellow"}
+dict2.update(dict1)
+print(dict2)
