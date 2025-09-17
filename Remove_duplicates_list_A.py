a94de6ea92026c66902bc1775bf242ff948ee2f2
Abhishek4128 <abhishekmandal0204@gmail.com>
2025-09-17 21:07:06 +0530

    Update Remove_duplicates_list_A.py

--- Remove_duplicates_list_A.py
+++ Remove_duplicates_list_A.py
@@ -1,7 +1,3 @@
-def removing_duplicates(lst):
-    unique_list=[]
-    for i in lst:
-        if i not in unique_list:
-            unique_list.append(i)
-    return unique_list
-print(removing_duplicates([2,2,3,5,3,6,7,1,5]))
+my_list=[2,2,3,5,3,6,7,1,5]
+my_list=list(set(my_list))
+print(my_list)
