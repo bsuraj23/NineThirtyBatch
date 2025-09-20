8a19a3905a179cc84792b76fb783a67afdd9dc29
prasadp2003 <prasadprasad11197@gmail.com>
2025-09-20 16:26:14 +0530

    Add files via upload

--- largest num in list.py
+++ largest num in list.py
@@ -1,2 +1,16 @@
-a=[1,2,54,67,78,90,69,96,]
-print(max(a))
+def second_largest(nums):
+    if len(nums) < 2:
+        return None  # Not enough elements
+
+    largest = second = float('-inf')
+
+    for num in nums:
+        if num > largest:
+            second = largest
+            largest = num
+        elif largest > num > second:
+            second = num
+
+    return second if second != float('-inf') else None
+print(second_largest([10, 20, 4, 45, 99]))   # Output: 45
+print(second_largest([5, 5, 5]))             # Output: None (no second largest)
