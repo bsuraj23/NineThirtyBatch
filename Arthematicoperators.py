779f825c38aced7e4fc7edad6cbf215d2554892e
Keerthananethikoppula <153708650+Keerthananethikoppula@users.noreply.github.com>
2025-10-16 22:17:15 +0530

    uploading files

---
+++ Arthematicoperators.py
@@ -0,0 +1,26 @@
+"""arthematic operators are +,-,*,/,// """
+A=10
+B=20
+sum=A+B
+print(sum)
+print("addition of A and B id",sum)
+
+sub=A-B
+print(sub)
+
+print("sub of a&b is",sub) 
+mul=A*B
+print(mul)
+
+
+result=A/B
+print(result)
+
+result=A//B
+print(result)
+
+result=A^B
+print(result)
+
+s=A**B
+print(s)
